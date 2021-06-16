7195a80e34f5f4aee25524cb4672fb60657692f9
tothpeter1216 <tothpeter1216@gmail.com>
2021-06-16 18:44:34 +0200

    mongodb connected by mongoose

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.use(express.json());
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("db connected");
+});
+
 app.get("/", (req, res) => {
   res.json({ info: "app works" });
 });
