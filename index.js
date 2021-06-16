f1451c150ed91e39afe975e20202f0d480fbad8f
tothpeter1216 <tothpeter1216@gmail.com>
2021-06-16 18:29:12 +0200

    server is ready

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ info: "app works" });
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
